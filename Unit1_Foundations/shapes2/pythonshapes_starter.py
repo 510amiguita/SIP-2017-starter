69f838f3ee98cfcb5f8da396835e229fc0150322
510amiguita <ornella.hs@alamedaclc.org>
2017-07-06 12:38:43 -0700

    Update pythonshapes_starter.py

--- Unit1_Foundations/shapes2/pythonshapes_starter.py
+++ Unit1_Foundations/shapes2/pythonshapes_starter.py
@@ -7,10 +7,27 @@ t = Turtle()
 # Set Up your screen and starting position.
 setup(500,300)
 x_pos = -250
-y_pos = -150
-t.setposition(x_pos, y_pos)
+y_pos = -100
 
-### Write your code below:
+color = input("Enter a color: ")
+
+if color == "red":
+    t.pencolor("red")
+elif color == "blue":
+    t.pencolor("blue")
+elif color == "green":
+    t.pencolor("green")
+elif color == "yellow":
+    t.pencolor("yellow")
+
+sides = 3;
+
+for movement in range(sides):
+    t.forward(100)
+    t.left(360/sides)
+
+
+
 
 
 
